c857e3ff656f2309413adfd845897b968673bf3c
ChatSites™ <info@chatsites.io>
2025-03-02 14:47:28 -0700

    Update index.ts

--- src/app/agentConfigs/index.ts
+++ src/app/agentConfigs/index.ts
@@ -1,19 +1,19 @@
 import chatSitesAttendant from "./chatSitesAttendant/chatSitesAttendant";
-import { realEstateAgents } from "./realEstateAgents/realEstateAgents";
-import { ecomAgents } from "./ecomAgents/ecomAgents";
-import { healthcareAgents } from "./healthcareAgents/healthcareAgents";
-import { financeAgents } from "./financeAgents/financeAgents";
-import { educationAgents } from "./educationAgents/educationAgents";
+import realEstateAgents from "./realEstateAgents/realEstateAgents";
+import ecomAgents from "./ecomAgents/ecomAgents";
+import educationAgents from "./educationAgents/educationAgents";
+import financeAgents from "./financeAgents/financeAgents";
+import healthcareAgents from "./healthcareAgents/healthcareAgents";
 
-export const allAgentSets: Record<string, AgentConfig[]> = {
-  chatSites: chatSitesAttendant,
-  realEstate: realEstateAgents,
-  ecommerce: ecomAgents,
-  healthcare: healthcareAgents,
-  finance: financeAgents,
-  education: educationAgents,
+
+export const allAgentSets: Record<string, any> = {
+  "ChatSites Attendant": chatSitesAttendant,
+  "Real Estate": realEstateAgents,
+  "E-commerce": ecomAgents,
+  "Healthcare": healthcareAgents,
+  "Finance": financeAgents,
+  "Education": educationAgents,
+  
 };
 
-
-export const defaultAgentSetKey = "chatSites";
-
+export const defaultAgentSetKey = Object.keys(allAgentSets)[0];
